caa6599cb6469d29442ca0a4af34e622fcc77af7
George Chen <georgc@iqmetrix.com>
2023-01-30 21:26:55 -0600

    checkout view

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -2,65 +2,76 @@
 
 import { reactive, ref , onMounted, nextTick} from "vue";
 const state = reactive ({
-  model: null, 
   isLoading : false,
   products: [],
   labels: []
 })
+
 const camera = ref(null)
 const canvas = ref(null)
-const model = ref(null)
+
+let model = null
 
 onMounted(async () => {
   state.isLoading = true
-  await loadLabels()
+  state.labels =await loadLabels()
+  model = await loadModel() 
   await openCamera()
   state.isLoading = false
 })
 
 let loadLabels = async () => {
   let resp = await fetch('v4/labels.txt')
-  state.labels = (await resp.text()).split("\n")
+  return (await resp.text()).split("\n")
 };
 
-async function checkout() {
+let loadModel = async () => {
   let model = new cvstfjs.ObjectDetectionModel();
   await model.loadModelAsync("/v4/model.json");
+  return model
+}
+
+async function checkout() {
   let tensor = tf.browser.fromPixels(camera.value, 3)
 		.resizeNearestNeighbor([416, 416]) // change the image size
 		.expandDims()
 		.toFloat()
 		.reverse(-1); // RGB -> BGR
+
   let data = await model.executeAsync(tensor);
+
+  const zip = (arr, ...arrs) => {
+    return arr.map((val, i) => arrs.reduce((a, arr) => [...a, arr[i]], [val]));
+  }
+  
   let products = zip(...data)
     .filter(d => d[1] > 0.5)
     .map(p => [...p, state.labels[p[2]]])
+
   console.log(products);
 }
 
-const draw = () => {
-  const context = canvas.value.getContext('2d');
-  context.drawImage(camera.value,0,0);
+const openCamera = async () => {
+  const openMediaDevices = async (constraints) => {
+    return await navigator.mediaDevices.getUserMedia(constraints);
+  };
+
+  try {
+      const stream = await openMediaDevices({'video':true,'audio':false});
+      camera.value.srcObject = stream
+  } catch(error) {
+      console.error('Error accessing camera.', error);
+  }
+
+  const draw = () => {
+    const context = canvas.value.getContext('2d');
+    context.drawImage(camera.value,0,0);
+    window.requestAnimationFrame(draw);
+  }
+
   window.requestAnimationFrame(draw);
 }
 
-const zip = (arr, ...arrs) => {
-  return arr.map((val, i) => arrs.reduce((a, arr) => [...a, arr[i]], [val]));
-}
-
-const openCamera = async () => {
-    const openMediaDevices = async (constraints) => {
-      return await navigator.mediaDevices.getUserMedia(constraints);
-    };
-
-    try {
-        const stream = await openMediaDevices({'video':true,'audio':false});
-        camera.value.srcObject = stream
-    } catch(error) {
-        console.error('Error accessing camera.', error);
-    }
-    window.requestAnimationFrame(draw);
-}
 </script>
 
 <template>
