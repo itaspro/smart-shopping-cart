fe7af33194b2cab2bb28e82a2ede0b02ef7639ac
George Chen <georgc@iqmetrix.com>
2023-02-05 14:05:38 -0600

    backend

--- src/components/TrainingItem.vue
+++ src/components/TrainingItem.vue
@@ -2,18 +2,13 @@
   import {  ref,reactive, onMounted, computed, watch  } from "vue";
   const pic = ref(null)
   const props = defineProps(['item'])
-  onMounted(() =>{
-  })
 </script>
 
 <template>
-
   <v-row>
-
       <h2>{{ props.item.label }}</h2>
   </v-row>
   <v-row>
-      <!-- <canvas ref="pic" id="canvas" ></canvas> -->
       <img class="training-images" v-for="item in props.item.items" :src="item.image" :key="item.label"/>
   </v-row>
 </template>
