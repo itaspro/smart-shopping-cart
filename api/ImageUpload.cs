c312bb5f42780788a85cc98759443751238d4520
George Chen <monstercoder@gmail.com>
2023-02-07 15:15:41 -0600

    response msg

--- api/ImageUpload.cs
+++ api/ImageUpload.cs
@@ -7,6 +7,8 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Azure.Storage.Blobs;
+using System.Collections.Generic;
+using Azure.Storage.Blobs.Models;
 
 namespace smart.Function
 {
@@ -17,16 +19,29 @@ namespace smart.Function
             [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req,
             ILogger log)
         {
-            string Connection = Environment.GetEnvironmentVariable("AzureWebJobsStorage");
+            string Connection = Environment.GetEnvironmentVariable("AzBlobStorage");
             string containerName = Environment.GetEnvironmentVariable("ContainerName");
+            var label = req.Headers["label"].ToString();
+            var argument = req.Headers["argument"];
+            var metaData = new Dictionary<string, string>() { {"label", label}};
             var blobClient = new BlobContainerClient(Connection, containerName);
-            var blob = blobClient.GetBlobClient("testfile.json");
-            var resp = await blob.UploadAsync(req.Body);
-            
-            string responseMessage = $"image upload";
-            log.LogInformation(responseMessage);
-
-            return new OkObjectResult(responseMessage);
+            var blob = blobClient.GetBlobClient($"{label}__{Path.GetRandomFileName()}");
+            BlobUploadOptions options = new BlobUploadOptions {
+                Metadata = metaData
+            };
+            var resp = await blob.UploadAsync(req.Body, options);
+            var http = resp.GetRawResponse();
+            if (http.Status < 300) {
+                 return new OkObjectResult("uploaded");
+            } else {
+                using(var read = new StreamReader(http.ContentStream)) {
+                    var msg = read.ReadToEnd();
+                    log.LogInformation(msg);
+                    var result = new ObjectResult(msg);
+                    result.StatusCode = http.Status;
+                    return result;
+                }
+            }
         }
     }
 }
